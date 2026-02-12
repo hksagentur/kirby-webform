4cc49645f45f5936ded5b01ede7f914991683abb
Daniel Weidner <d.weidner@hks-agentur.de>
2026-02-12 17:22:58 +0100

    feat: add support for webform introduction text

--- snippets/blocks/webform.php
+++ snippets/blocks/webform.php
@@ -4,13 +4,20 @@
     'id' => $block->form()->getId(),
     'class' => [
         'webform',
-        'webform--'.$block->form()->getId()
+        'webform--'.$block->form()->getId(),
+        'stack',
     ],
 ]) ?>>
     <?php if ($block->title()->isNotEmpty()) : ?>
-        <h2 class="webform__title heading heading--md">
+        <h2 class="webform__title">
             <?= $block->title()->esc() ?>
         </h2>
+    <?php endif ?>
+
+    <?php if ($block->text()->isNotEmpty()) : ?>
+        <div class="webform__text">
+            <?= $block->text()->kirbytext() ?>
+        </div>
     <?php endif ?>
 
     <?= $block->form() ?>
